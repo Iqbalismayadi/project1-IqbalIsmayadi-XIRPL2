9077e16a922d602d462a87df44f8c11b88792379
iqbal ismayadi <iqbalismayadi12@gmail.com>
2021-03-03 10:04:41 +0700

    implementasi kelas Ramen, Kuah, Toping dan Minuman

--- Aplikasirestauran/src/classes/Minuman.java
+++ Aplikasirestauran/src/classes/Minuman.java
@@ -10,5 +10,10 @@ package classes;
  * @author acer
  */
 public class Minuman extends Menu{
-    public Minuman(String nama_minuman, double harga) {}
+    
+    public Minuman(String nama_minuman, double harga) {
+        setNama_menu (nama_minuman);
+        setHarga(harga);
+        setKategori ("Minuman");
+    }
 }
